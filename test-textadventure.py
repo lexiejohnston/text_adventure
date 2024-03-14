89d94f37fe62d0e136625392a888484fb21eba75
WillGS-TPC <149803934+WillGS-TPC@users.noreply.github.com>
2024-03-14 13:50:19 +0000

    Update test-textadventure.py

--- test-textadventure.py
+++ test-textadventure.py
@@ -7,5 +7,8 @@ from unittest.mock import Mock
 from textadventure import *
  
 class RichRoomTest(unittest.TestCase):
-    def a_rich_room_can_be_created(self):
+    def test_a_rich_room_can_be_created(self):
         room = RichRoom(None)
+     
+if __name__ == '__main__':
+ unittest.main()
